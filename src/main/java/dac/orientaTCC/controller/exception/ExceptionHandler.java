c3a54d773c8edc5b68178ab390c060e365229be6
user.name <anafelix0909@gmail.com>
2025-06-29 14:41:26 -0300

    feat: implementação das classes de erro, ErroMessage e ExceptionHandler

---
+++ src/main/java/dac/orientaTCC/controller/exception/ExceptionHandler.java
@@ -0,0 +1,7 @@
+package dac.orientaTCC.controller.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandler { //captura todas as exception aqui
+}
