a342b67744b8d4f373741c2964405360a423285f
Emerson Andrey <eandrey413@gmail.com>
2025-06-30 00:08:50 -0300

    feat: implementando logica de enviar o email com nova senha para o usuario

---
+++ src/main/java/dac/orientaTCC/controller/EmailController.java
@@ -0,0 +1,26 @@
+package dac.orientaTCC.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dac.orientaTCC.service.EmailService;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/enviar")
+    public String send(@RequestParam String to, @RequestParam String nome) {
+        emailService.enviarEmail(to, nome);
+        return "Enviado para " + to;
+    }
+
+}
